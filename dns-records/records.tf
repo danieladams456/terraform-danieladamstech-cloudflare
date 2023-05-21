b0f0b8b89b1bdaad6f089fe6885a8f0597dd3cf1
Daniel Adams <daniel.adams@enactmi.com>
2023-05-20 22:43:15 -0400

    create dns records project

---
+++ dns-records/records.tf
@@ -0,0 +1,9 @@
+# DNS record must exist to reach redirects, docs say to use 192.0.2.1
+# https://developers.cloudflare.com/pages/how-to/www-redirect/
+resource "cloudflare_record" "apex" {
+  zone_id = data.cloudflare_zone.danieladamstech.id
+  name    = "@"
+  value   = "192.0.2.1"
+  type    = "A"
+  ttl     = 3600
+}
