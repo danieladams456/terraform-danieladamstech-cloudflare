86400d2ee9644787bf936e8c075d43bf467528d8
Daniel Adams <daniel.adams@enactmi.com>
2023-05-21 21:18:51 -0400

    dns for apex and blog A and AAAA

--- dns-records/records.tf
+++ dns-records/records.tf
@@ -1,10 +1,21 @@
 # DNS record must exist to reach redirects, docs say to use 192.0.2.1
 # https://developers.cloudflare.com/pages/how-to/www-redirect/
-resource "cloudflare_record" "apex" {
-  zone_id = data.cloudflare_zone.danieladamstech.id
-  name    = "@"
-  value   = "192.0.2.1"
-  type    = "A"
-  ttl     = 1
-  proxied = true
+resource "cloudflare_record" "pages_a" {
+  for_each = local.pages_records
+  zone_id  = data.cloudflare_zone.danieladamstech.id
+  name     = each.value
+  value    = "192.0.2.1"
+  type     = "A"
+  ttl      = 1
+  proxied  = true
+}
+
+resource "cloudflare_record" "pages_aaaa" {
+  for_each = local.pages_records
+  zone_id  = data.cloudflare_zone.danieladamstech.id
+  name     = each.value
+  value    = "100::"
+  type     = "AAAA"
+  ttl      = 1
+  proxied  = true
 }
---
+++ dns-records/variables.tf
@@ -0,0 +1,3 @@
+locals {
+  pages_records = ["@", "blog"]
+}
