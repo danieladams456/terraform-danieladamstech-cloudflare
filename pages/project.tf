5f2b5fd56e9eceb12f03f68c4b41153ea10b637e
Daniel Adams <daniel.adams@enactmi.com>
2023-05-26 14:06:50 -0400

    build settings for Pages

--- pages/project.tf
+++ pages/project.tf
@@ -6,9 +6,30 @@ resource "cloudflare_pages_project" "blog" {
   source {
     type = "github"
     config {
-      owner             = "danieladams456"
-      repo_name         = "blog-danieladamstech"
-      production_branch = "main"
+      owner                   = "danieladams456"
+      repo_name               = "blog-danieladamstech"
+      production_branch       = "main"
+      preview_branch_includes = ["*"]
+    }
+  }
+
+  build_config {
+    build_command   = "hugo"
+    destination_dir = "public"
+  }
+
+  deployment_configs {
+    preview {
+      always_use_latest_compatibility_date = true
+      environment_variables = {
+        HUGO_VERSION = "0.112.3"
+      }
+    }
+    production {
+      compatibility_date = "2023-03-14"
+      environment_variables = {
+        HUGO_VERSION = "0.112.3"
+      }
     }
   }
 }
