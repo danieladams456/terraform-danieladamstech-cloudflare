3e85f951a317c37106c28ec10463248ce46db29f
Daniel Adams <daniel.adams@enactmi.com>
2023-05-26 13:24:43 -0400

    add source repo attributes to pages project

--- pages/domains.tf
+++ pages/domains.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_pages_domain" "blog" {
   account_id   = local.account_id
-  project_name = cloudflare_pages_project.basic_project.name
+  project_name = cloudflare_pages_project.blog.name
   domain       = "blog.danieladamstech.com"
 }
--- pages/project.tf
+++ pages/project.tf
@@ -1,5 +1,14 @@
-resource "cloudflare_pages_project" "basic_project" {
+resource "cloudflare_pages_project" "blog" {
   account_id        = local.account_id
   name              = "blog-danieladamstech"
   production_branch = "main"
+
+  source {
+    type = "github"
+    config {
+      owner             = "danieladams456"
+      repo_name         = "blog-danieladamstech"
+      production_branch = "main"
+    }
+  }
 }
